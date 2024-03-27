9fa8b366345b94572621b99c9bf7974c640e42da
jybium <asaolujtomi@gmail.com>
2024-03-27 05:08:26 +0100

    Fix for prisma type error

--- src/app/api/create/route.js
+++ src/app/api/create/route.js
@@ -5,8 +5,8 @@ import { createClient } from "@supabase/supabase-js";
 
 // Create Supabase client
 const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_API_KEY
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_ANON_KEY
 );
 
 export async function POST(request) {
